23647b7743fe815ea911fe74f908329ac5a7e472
KenSpur <spur.ken@gmail.com>
2023-09-29 10:58:24 +0200

    account for reverse proxies

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", async context => {
-  var ipAddress = context.Connection.RemoteIpAddress.MapToIPv4().ToString();
+  var headers = context.Request.Headers;
+  var ipAddress = headers.ContainsKey("X-Forwarded-For") 
+    ? headers["X-Forwarded-For"].FirstOrDefault()
+    : context.Connection.RemoteIpAddress?.MapToIPv4().ToString();
   await context.Response.WriteAsJsonAsync(new { IpAddress = ipAddress });
 });
 
