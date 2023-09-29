e357e57ec4d55ae090cf5e98beb35b62e2b4ed04
KenSpur <spur.ken@gmail.com>
2023-09-29 11:30:33 +0200

    account for multiple

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 app.MapGet("/", async context => {
   var headers = context.Request.Headers;
   var ipAddress = headers.ContainsKey("X-Forwarded-For") 
-    ? headers["X-Forwarded-For"].FirstOrDefault()
+    ? headers["X-Forwarded-For"].FirstOrDefault()?.Split(',').FirstOrDefault()?.Trim()
     : context.Connection.RemoteIpAddress?.MapToIPv4().ToString();
   await context.Response.WriteAsJsonAsync(new { IpAddress = ipAddress });
 });
