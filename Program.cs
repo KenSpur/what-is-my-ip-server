0106265acabd9ab13b4cf7efae5d7fbfd6e78c65
KenSpur <spur.ken@gmail.com>
2023-09-29 10:42:12 +0200

    map to ip v4

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", async context => {
-  var ipAddress = context.Connection.RemoteIpAddress.ToString();
+  var ipAddress = context.Connection.RemoteIpAddress.MapToIPv4().ToString();
   await context.Response.WriteAsJsonAsync(new { IpAddress = ipAddress });
 });
 
